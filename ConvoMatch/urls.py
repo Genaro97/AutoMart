14d03cc2cfffd870abf51f369b8d3357b1e2d58d
Genaro <galdana5189@gmail.com>
2019-10-01 19:38:22 -0500

    Primer commit

--- ConvoMatch/urls.py
+++ ConvoMatch/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', include('main_app.urls')),
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,22 +1,7 @@
 from django.shortcuts import render
+from .models import Auto
 
 # Create your views here.
 def index(request):
-
-
-    return render(request, 'index.html',{'autos' : autos})
-
-    class Auto:
-        def __init__(self, nombre, precio, modelo, color):
-            self.nombre = nombre
-            self.precio = precio
-            self.modelo = modelo
-            self.color = color
-
-
-autos = [
-    Auto('VW Jetta', 145000, 2018, "Gris"),
-    Auto("Lexus", 256000, 2017, "Rojo"),
-    Auto("Futura", 0, 1954, "Aqua"),
-    Auto("Porsche", 250000, 2010, "Azul")
-]
+    autos = Auto.objects.all()
+    return render(request, 'index.html', {'autos' : autos})
